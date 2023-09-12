9d476f9e367a63eab9204c9c70e9c54f4ea7085d
Shubham <shubhamdeshmukh3826@gmail.com>
2023-09-12 07:11:36 +0530

    AshteLogistics

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: Logis
-* Updated: Aug 30 2023 with Bootstrap v5.3.1
-* Template URL: https://bootstrapmade.com/logis-bootstrap-logistics-website-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
+
 */
 document.addEventListener('DOMContentLoaded', () => {
   "use strict";
